4865904c7bcc24276253b4384f14864a24e7076c
Nicholas Flintham <nick@flinny.org>
2012-08-21 14:54:50 +0100

    add audio.usb.default
    Change-Id: Ieaa75827454dddda8d01deb7d18a77937af9525b

--- msm7x30.mk
+++ msm7x30.mk
@@ -58,6 +58,7 @@ PRODUCT_PACKAGE_OVERLAYS += device/htc/msm7x30-common/overlay
 PRODUCT_PACKAGES += \
     audio.a2dp.default \
     audio.primary.msm7x30 \
+    audio.usb.default \
     libaudioutils \
     libtinyalsa
 
