bd3444bd0611c05b575a329b02b7d6de0c3f5361
Nicholas Flintham <nick@flinny.org>
2012-09-16 22:50:32 +0100

    Boardconfig cleanup and changes required to switch to caf msm7x30 audio
    Change-Id: Ida25f70aeb387ae7f99fa9fe0f4a9ec462d6c664

--- msm7x30.mk
+++ msm7x30.mk
@@ -46,8 +46,8 @@ PRODUCT_COPY_FILES += \
 # media configs
 PRODUCT_COPY_FILES += \
     device/htc/msm7x30-common/media_profiles.xml:system/etc/media_profiles.xml \
-    device/htc/msm7x30-common/media_codecs.xml:system/etc/media_codecs.xml \
-    device/htc/msm7x30-common/audio_policy.conf:system/etc/audio_policy.conf
+    device/htc/msm7x30-common/media_codecs.xml:system/etc/media_codecs.xml
+
 # Camera
 PRODUCT_COPY_FILES += \
     device/htc/msm7x30-common/prebuilt/libsurfaceflinger_client.so:system/lib/libsurfaceflinger_client.so
@@ -58,6 +58,8 @@ PRODUCT_PACKAGE_OVERLAYS += device/htc/msm7x30-common/overlay
 PRODUCT_PACKAGES += \
     audio.a2dp.default \
     audio.primary.msm7x30 \
+    audio_policy.msm7x30 \
+    audio_policy.conf \
     audio.usb.default \
     libaudioutils \
     libtinyalsa
