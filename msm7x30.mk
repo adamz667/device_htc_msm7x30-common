4b2a9686b4dc6cc74352c1f04443df203dcf9bce
Nicholas Flintham <nick@flinny.org>
2012-11-01 23:16:38 +0000

    build prop additions
    Change-Id: I60cdc8efed17a55dbfd9a02034e94270ec0592a4

--- msm7x30.mk
+++ msm7x30.mk
@@ -123,3 +123,9 @@ PRODUCT_AAPT_CONFIG := normal hdpi
 PRODUCT_AAPT_PREF_CONFIG := hdpi
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.composition.type=dyn \
+    debug.sf.hw=1 \
+    debug.egl.hw=1
+
